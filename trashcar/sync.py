d0329a025226ee5efb57866fae62bb8de22506a9
andycheng <ccjeng@gmail.com>
2015-12-14 12:05:33 +0800

    no message

--- trashcar/sync.py
+++ trashcar/sync.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 
-#from parse_rest.connection import register
-#from parse_rest.datatypes import Object, GeoPoint
-#from parse_rest.connection import ParseBatcher
-
-
 import codecs, json,httplib
 import urllib2
 import ast
 
-#APPLICATION_ID = "6M0xS8A8uaqXbGGP8GAVFCw8ah7B6cif0NlZhYm6"
-#REST_API_KEY = "ASDpoFdwFAdbRlI6bAH7X0onYK5xuE8XWwL9uafm"
-
 urlTaipei = 'http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=8f2e2264-6eab-451f-a66d-34aa2a0aa7b1'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000' #1~2000
-'''
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=2000' #2000~4000
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=4000' #4000~6000
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=6000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=8000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=10000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=12000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=14000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=16000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=18000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=20000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=22000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=24000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=26000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=28000'
-urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json&$top=2000&$skip=30000'
-'''
+urlNewTaipei = 'http://data.ntpc.gov.tw/od/data/api/EDC3AD26-8AE7-4916-A00B-BC6048D19BF8?$format=json'
+
+urlNewTaipeiList = ['&$top=2000' #1~2000
+, '&$top=2000&$skip=2000' #2000~4000
+, '&$top=2000&$skip=4000' #4000~6000
+, '&$top=2000&$skip=6000'
+, '&$top=2000&$skip=8000'
+, '&$top=2000&$skip=10000'
+, '&$top=2000&$skip=12000'
+, '&$top=2000&$skip=14000'
+, '&$top=2000&$skip=16000'
+, '&$top=2000&$skip=18000'
+, '&$top=2000&$skip=20000'
+, '&$top=2000&$skip=22000'
+, '&$top=2000&$skip=24000'
+, '&$top=2000&$skip=26000'
+, '&$top=2000&$skip=28000'
+, '&$top=2000&$skip=30000' #30000~32000
+]
+
 
 class Truck(object):
     def __init__(self, city, region, address, lineid, line, carno, time, hour, memo
@@ -89,13 +83,13 @@ items = data["result"]["results"]
 for item in items:
 	carTime=item['CarTime'].replace(u'：',':')
 	strHour=carTime[0:carTime.index(':')]
-	loc=ast.literal_eval('{"__type": "GeoPoint", "longitude":' + str(float(item['Lng'])) + ',"latitude":' + str(float(item['Lat'])) + ' }')
+	locationString=ast.literal_eval('{"__type": "GeoPoint", "longitude":' + str(float(item['Lng'])) + ',"latitude":' + str(float(item['Lat'])) + ' }')
 	#print loc
-	t = Truck('Taipei',item['Li'],item['Address'],'',item['CarNumber'],item['CarNo'],item['CarTime'],int(strHour),item['DepName']
+	t = Truck('Taipei',item['Li'],item['Address'],'',item['CarNumber'],item['CarNo'],item['CarTime'],strHour,item['DepName']
 		,'N','Y','Y','N','Y','Y','Y'
 		,'N','Y','Y','N','Y','Y','Y'
 		,'N','Y','Y','N','Y','Y','Y'
-		, loc
+		, locationString
 		)
 	jsonStringTruck = json.dumps(t.__dict__, ensure_ascii=False)
 	Trucks.append(ast.literal_eval(jsonStringTruck))
@@ -103,50 +97,46 @@ for item in items:
 
 
 # New Taipei
+for top in urlNewTaipeiList:
+	url = urlNewTaipei + top
+	print url
+	response = urllib2.urlopen(url).read().decode('utf8')
+	items = json.loads(response)
 
-response = urllib2.urlopen(urlNewTaipei).read().decode('utf8')
+	#import data
+	for item in items:
+		strHour=item['time'][0:item['time'].index(':')]
+		locationString=ast.literal_eval('{"__type": "GeoPoint", "longitude":' + str(float(item['longitude'])) + ',"latitude":' + str(float(item['latitude'])) + ' }')
+		t = Truck('NewTaipei',item['village'],item['name'],item['lineid'],item['linename'],item['rank'],item['time'],strHour,item['memo']
+			,(item['garbage_sun'] if item['garbage_sun']=='Y' else 'N')
+			,(item['garbage_mon'] if item['garbage_mon']=='Y' else 'N')
+			,(item['garbage_tue'] if item['garbage_tue']=='Y' else 'N')
+			,(item['garbage_wed'] if item['garbage_wed']=='Y' else 'N')
+			,(item['garbage_thu'] if item['garbage_thu']=='Y' else 'N')
+			,(item['garbage_fri'] if item['garbage_fri']=='Y' else 'N')
+			,(item['garbage_sat'] if item['garbage_sat']=='Y' else 'N')
+			,(item['recycling_sun'] if item['recycling_sun']=='Y' else 'N')
+			,(item['recycling_mon'] if item['recycling_mon']=='Y' else 'N')
+			,(item['recycling_tue'] if item['recycling_tue']=='Y' else 'N')
+			,(item['recycling_wed'] if item['recycling_wed']=='Y' else 'N')
+			,(item['recycling_thu'] if item['recycling_thu']=='Y' else 'N')
+			,(item['recycling_fri'] if item['recycling_fri']=='Y' else 'N')
+			,(item['recycling_sat'] if item['recycling_sat']=='Y' else 'N')
+			,(item['foodscraps_sun'] if item['foodscraps_sun']=='Y' else 'N')
+			,(item['foodscraps_mon'] if item['foodscraps_mon']=='Y' else 'N')
+			,(item['foodscraps_tue'] if item['foodscraps_tue']=='Y' else 'N')
+			,(item['foodscraps_wed'] if item['foodscraps_wed']=='Y' else 'N')
+			,(item['foodscraps_thu'] if item['foodscraps_thu']=='Y' else 'N')
+			,(item['foodscraps_fri'] if item['foodscraps_fri']=='Y' else 'N')
+			,(item['foodscraps_sat'] if item['foodscraps_sat']=='Y' else 'N')
+			, locationString
+			)
+		jsonStringTruck = json.dumps(t.__dict__, ensure_ascii=False)
+		Trucks.append(ast.literal_eval(jsonStringTruck))
 
-data = json.loads(response)
-
-items = data
-
-#import data
-for item in items:
-	strHour=item['time'][0:item['time'].index(':')]
-	loc=ast.literal_eval('{"__type": "GeoPoint", "longitude":' + str(float(item['longitude'])) + ',"latitude":' + str(float(item['latitude'])) + ' }')
-	t = Truck('NewTaipei',item['village'],item['name'],item['lineid'],item['linename'],item['rank'],item['time'],int(strHour),item['memo']
-		,(item['garbage_sun'] if item['garbage_sun']=='Y' else 'N')
-		,(item['garbage_mon'] if item['garbage_mon']=='Y' else 'N')
-		,(item['garbage_tue'] if item['garbage_tue']=='Y' else 'N')
-		,(item['garbage_wed'] if item['garbage_wed']=='Y' else 'N')
-		,(item['garbage_thu'] if item['garbage_thu']=='Y' else 'N')
-		,(item['garbage_fri'] if item['garbage_fri']=='Y' else 'N')
-		,(item['garbage_sat'] if item['garbage_sat']=='Y' else 'N')
-		,(item['recycling_sun'] if item['recycling_sun']=='Y' else 'N')
-		,(item['recycling_mon'] if item['recycling_mon']=='Y' else 'N')
-		,(item['recycling_tue'] if item['recycling_tue']=='Y' else 'N')
-		,(item['recycling_wed'] if item['recycling_wed']=='Y' else 'N')
-		,(item['recycling_thu'] if item['recycling_thu']=='Y' else 'N')
-		,(item['recycling_fri'] if item['recycling_fri']=='Y' else 'N')
-		,(item['recycling_sat'] if item['recycling_sat']=='Y' else 'N')
-		,(item['foodscraps_sun'] if item['foodscraps_sun']=='Y' else 'N')
-		,(item['foodscraps_mon'] if item['foodscraps_mon']=='Y' else 'N')
-		,(item['foodscraps_tue'] if item['foodscraps_tue']=='Y' else 'N')
-		,(item['foodscraps_wed'] if item['foodscraps_wed']=='Y' else 'N')
-		,(item['foodscraps_thu'] if item['foodscraps_thu']=='Y' else 'N')
-		,(item['foodscraps_fri'] if item['foodscraps_fri']=='Y' else 'N')
-		,(item['foodscraps_sat'] if item['foodscraps_sat']=='Y' else 'N')
-		, loc
-		)
-	jsonStringTruck = json.dumps(t.__dict__, ensure_ascii=False)
-	Trucks.append(ast.literal_eval(jsonStringTruck))
-
-
-
-#print Trucks
 json_string = '{"results":' + json.dumps(Trucks, ensure_ascii=False) + '}'
-#print json_string
 
+#Write to Json File
 with codecs.open("TPE.json", "w") as outfile:
 	outfile.write(json_string)
 	#outfile.write(json_string.decode('utf8'))
