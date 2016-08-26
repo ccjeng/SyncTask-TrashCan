e37b50945a4e10eef99a257447a81fcd962b3df8
andycheng <ccjeng@gmail.com>
2016-08-26 10:49:25 +0800

    no message

--- trashcan/sync-firebase.py
+++ trashcan/sync-firebase.py
@@ -8,7 +8,18 @@ import ast
 
 root = 'http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid='
 
-dict = {u'士林區': '97cc923a-e9ee-4adc-8c3d-335567dc15d3', u'大同區': '5fa14e06-018b-4851-8316-1ff324384f79', u'大安區': 'f40cd66c-afba-4409-9289-e677b6b8d00e', u'中山區': '33b2c4c5-9870-4ee9-b280-a3a297c56a22', u'中正區': '0b544701-fb47-4fa9-90f1-15b1987da0f5', u'內湖區': '37eac6d1-6569-43c9-9fcf-fc676417c2cd', u'文山區': '46647394-d47f-4a4d-b0f0-14a60ac2aade', u'北投區': '05d67de9-a034-4177-9f53-10d6f79e02cf', u'松山區': '179d0fe1-ef31-4775-b9f0-c17b3adf0fbc', u'信義區': '8cbb344b-83d2-4176-9abd-d84508e7dc73', u'南港區': '7b955414-f460-4472-b1a8-44819f74dc86', u'萬華區': '5697d81f-7c9d-43fc-a202-ae8804bbd34b'
+dict = {u'士林區': '97cc923a-e9ee-4adc-8c3d-335567dc15d3'
+, u'大同區': '5fa14e06-018b-4851-8316-1ff324384f79'
+, u'大安區': 'f40cd66c-afba-4409-9289-e677b6b8d00e'
+, u'中山區': '33b2c4c5-9870-4ee9-b280-a3a297c56a22'
+, u'中正區': '0b544701-fb47-4fa9-90f1-15b1987da0f5'
+, u'內湖區': '37eac6d1-6569-43c9-9fcf-fc676417c2cd'
+, u'文山區': '46647394-d47f-4a4d-b0f0-14a60ac2aade'
+, u'北投區': '05d67de9-a034-4177-9f53-10d6f79e02cf'
+, u'松山區': '179d0fe1-ef31-4775-b9f0-c17b3adf0fbc'
+, u'信義區': '8cbb344b-83d2-4176-9abd-d84508e7dc73'
+, u'南港區': '7b955414-f460-4472-b1a8-44819f74dc86'
+, u'萬華區': '5697d81f-7c9d-43fc-a202-ae8804bbd34b'
         }
 
 class TrashCan(object):
@@ -19,24 +30,25 @@ class TrashCan(object):
         self.longitude = longitude
 
 TrashCans = []
-count = 0;
+count = 0
 
 for key, value in dict.items():
-
     url = root + value
-
     print(key)
-
     response = requests.get(url)
     items = response.json()["result"]["results"]
 
     for item in items:
-        if item[u'路名'] is not None:
-            t = TrashCan(key, item[u'路名'] + item[u'段、號及其他註明'], str(float(item[u'緯度'].replace(',', '').replace('25.02.49', '25.0249'))), str(float(item[u'經度'].replace('121.566.489', '121.566489').replace('121.33.28', '121.3328').replace('121..549132', '121.549132')))
+        if not item[u'緯度'].strip() == '':
+            t = TrashCan(key, item[u'路名'] + item[u'段、號及其他註明']
+            , str(float(item[u'緯度'].replace(',', '').replace('25.02.49', '25.0249')))
+            , str(float(item[u'經度'].replace('121.566.489', '121.566489').replace('121.33.28', '121.3328').replace('121..549132', '121.549132')))
                          )
             jsonStringTruck = json.dumps(t.__dict__, ensure_ascii=False)
             TrashCans.append(ast.literal_eval(jsonStringTruck))
-            count = count + 1;
+            count = count + 1
+        else:
+            print(item[u'路名'] + '-' + item[u'緯度'] + '-' +item[u'經度'])
 
 print('Total Count = ' + str(count))
 
